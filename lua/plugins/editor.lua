c342396bd5e55c3fb7f5880c66326ded313a1985
pwjworks <13610513466@163.com>
2023-03-25 22:06:56 +0800

    add cmp highlight

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -457,11 +457,48 @@ return {
     "hrsh7th/nvim-cmp",
     dependencies = { "hrsh7th/cmp-emoji",
       "onsails/lspkind.nvim",
-      "nvim-tree/nvim-web-devicons"
     },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
+      local hl_groups = {
+        PmenuSel = { bg = "#282C34", fg = "NONE" },
+        Pmenu = { fg = "#C5CDD9", bg = "#22252A" },
+        CmpItemAbbrDeprecated = { fg = "#7E8294", bg = "NONE" },
+        CmpItemAbbrMatch = { fg = "#82AAFF", bg = "NONE" },
+        CmpItemAbbrMatchFuzzy = { fg = "#82AAFF", bg = "NONE" },
+        CmpItemMenu = { fg = "#C792EA", bg = "NONE" },
+        CmpItemKindField = { fg = "#EED8DA", bg = "#B5585F" },
+        CmpItemKindProperty = { fg = "#EED8DA", bg = "#B5585F" },
+        CmpItemKindEvent = { fg = "#EED8DA", bg = "#B5585F" },
+        CmpItemKindText = { fg = "#C3E88D", bg = "#9FBD73" },
+        CmpItemKindEnum = { fg = "#C3E88D", bg = "#9FBD73" },
+        CmpItemKindKeyword = { fg = "#C3E88D", bg = "#9FBD73" },
+        CmpItemKindConstant = { fg = "#FFE082", bg = "#D4BB6C" },
+        CmpItemKindConstructor = { fg = "#FFE082", bg = "#D4BB6C" },
+        CmpItemKindReference = { fg = "#FFE082", bg = "#D4BB6C" },
+        CmpItemKindFunction = { fg = "#EADFF0", bg = "#A377BF" },
+        CmpItemKindStruct = { fg = "#EADFF0", bg = "#A377BF" },
+        CmpItemKindClass = { fg = "#EADFF0", bg = "#A377BF" },
+        CmpItemKindModule = { fg = "#EADFF0", bg = "#A377BF" },
+        CmpItemKindOperator = { fg = "#EADFF0", bg = "#A377BF" },
+        CmpItemKindVariable = { fg = "#C5CDD9", bg = "#7E8294" },
+        CmpItemKindFile = { fg = "#C5CDD9", bg = "#7E8294" },
+        CmpItemKindUnit = { fg = "#F5EBD9", bg = "#D4A959" },
+        CmpItemKindSnippet = { fg = "#F5EBD9", bg = "#D4A959" },
+        CmpItemKindFolder = { fg = "#F5EBD9", bg = "#D4A959" },
+        CmpItemKindMethod = { fg = "#DDE5F5", bg = "#6C8ED4" },
+        CmpItemKindValue = { fg = "#DDE5F5", bg = "#6C8ED4" },
+        CmpItemKindEnumMember = { fg = "#DDE5F5", bg = "#6C8ED4" },
+        CmpItemKindInterface = { fg = "#D8EEEB", bg = "#58B5A8" },
+        CmpItemKindColor = { fg = "#D8EEEB", bg = "#58B5A8" },
+        CmpItemKindTypeParameter = { fg = "#D8EEEB", bg = "#58B5A8" },
+      }
+      for group, color in pairs(hl_groups) do
+        vim.api.nvim_set_hl(0, group, color)
+      end
+      vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#C792EA", bold = true })
+
       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
       opts.window = {
         completion = {
@@ -476,22 +513,19 @@ return {
       opts.formatting = {
         fields = { "kind", "abbr", "menu" },
         format = function(entry, vim_item)
-          if vim.tbl_contains({ 'path' }, entry.source.name) then
-            local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
-            if icon then
-              vim_item.kind = icon
-              vim_item.kind_hl_group = hl_group
-              return vim_item
-            end
-          end
           local kind = require("lspkind").cmp_format({
             mode = "symbol_text",
             maxwidth = 60,
           })(entry, vim_item)
           local strings = vim.split(kind.kind, "%s", { trimempty = true })
+          local item = entry:get_completion_item()
 
+          if item.detail then
+            kind.menu = "    (" .. (strings[2] or "") .. ")✨" .. item.detail
+          else
+            kind.menu = "    (" .. (strings[2] or "") .. ")"
+          end
           kind.kind = " " .. (strings[1] or "") .. " "
-          kind.menu = "    (" .. (strings[2] or "") .. ")"
           return kind
         end,
       }
