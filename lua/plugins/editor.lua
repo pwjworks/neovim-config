38809af52b2d30cfe265f69c78f1d11a0b41bc32
pwjworks <13610513466@163.com>
2023-03-30 14:55:56 +0800

    add window-picker

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -198,5 +198,57 @@ return {
     config = function()
       require("better_escape").setup()
     end,
-  }
+  },
+  {
+    "Kohirus/cppassist.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("cppassist").setup()
+    end
+  },
+  {
+    "s1n7ax/nvim-window-picker",
+    lazy = true,
+    event = { "WinNew" },
+    config = function()
+      local picker = require("window-picker")
+      picker.setup({
+        autoselect_one = true,
+        include_current = false,
+        filter_rules = {
+          bo = {
+            filetype = { "neo-tree", "neo-tree-popup", "notify", "quickfix" },
+            buftype = { "terminal" },
+          },
+        },
+        other_win_hl_color = "#e35e4f",
+      })
+
+      vim.keymap.set("n", ",w", function()
+        local picked_window_id = picker.pick_window({
+              include_current_win = true,
+            }) or vim.api.nvim_get_current_win()
+        vim.api.nvim_set_current_win(picked_window_id)
+      end, { desc = "Pick a window" })
+
+      -- Swap two windows using the awesome window picker
+      local function swap_windows()
+        local window = picker.pick_window({
+          include_current_win = false,
+        })
+        local target_buffer = vim.fn.winbufnr(window)
+        -- Set the target window to contain current buffer
+        vim.api.nvim_win_set_buf(window, 0)
+        -- Set current window to contain target buffer
+        vim.api.nvim_win_set_buf(0, target_buffer)
+      end
+
+      vim.keymap.set("n", ",W", swap_windows, { desc = "Swap windows" })
+    end,
+  },
+  {
+    "nvim-zh/colorful-winsep.nvim",
+    config = true,
+    event = { "WinNew" },
+  },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,7 +32,6 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     dependencies = { "mason.nvim" },
     opts = function()
-      local nls = require("null-ls")
       return {
         root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
         sources = {},
@@ -79,6 +78,7 @@ return {
   },
   {
     "Kohirus/cppassist.nvim",
+    event = "VeryLazy",
     config = function()
       require("cppassist").setup()
     end
