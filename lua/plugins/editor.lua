609f5f840d8ec1f1fa88e8a54460e72d57ec39f0
pwjworks <13610513466@163.com>
2023-04-03 16:48:39 +0800

    add hop.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -263,4 +263,17 @@ return {
     config = true,
     event = { "WinNew" },
   },
+  {
+    "phaazon/hop.nvim",
+    lazy = true,
+    keys = { "E" },
+    config = function()
+      require("hop").setup({})
+      -- vim.api.nvim_set_keymap("n", "R", "<cmd>HopChar2<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "E", "<cmd>HopChar1<cr>", { silent = true })
+      -- vim.api.nvim_set_keymap("n", "U", "<cmd>HopWord<cr>", { silent = true })
+      -- vim.api.nvim_set_keymap("n", "C", "<cmd>HopLine<cr>", { silent = true })
+      -- vim.api.nvim_set_keymap("n", "P", "<cmd>HopPattern<cr>", { silent = true })
+    end,
+  },
 }
