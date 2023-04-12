1d9f0e1212337b3ceb052d4c2f9cc86e1ce86b9c
pwjworks <13610513466@163.com>
2023-04-13 03:03:11 +0800

    fix treesitter

--- lua/plugins/config/treesitter.lua
+++ lua/plugins/config/treesitter.lua
@@ -1,32 +1,4 @@
 local options = {
-  ensure_installed = {
-    "bash",
-    "comment",
-    "diff",
-    "dockerfile",
-    "dot",
-    "git_rebase",
-    "gitattributes",
-    "gitcommit",
-    "gitignore",
-    "lua",
-    "make",
-    "markdown",
-    "markdown_inline",
-    "mermaid",
-    "python",
-    "regex",
-    "vim",
-    "yaml",
-    "cpp",
-  },
-  rainbow = {
-    enable = true,
-    -- Which query to use for finding delimiters
-    query = 'rainbow-parens',
-    -- Highlight the entire buffer all at once
-    strategy = require('ts-rainbow').strategy.global,
-  }
 }
 
 return options
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -72,9 +72,36 @@ return {
       "HiPhish/nvim-ts-rainbow2",
       "nvim-treesitter/playground"
     },
-    config = function()
-      require("nvim-treesitter.configs").setup(require("plugins.config.treesitter"))
-    end
+    opts = function(_, opts)
+      opts.ensure_installed = {
+        "bash",
+        "comment",
+        "diff",
+        "dockerfile",
+        "dot",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "lua",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "mermaid",
+        "python",
+        "regex",
+        "vim",
+        "yaml",
+        "cpp",
+      }
+      opts.rainbow = {
+        enable = true,
+        -- Which query to use for finding delimiters
+        query = 'rainbow-parens',
+        -- Highlight the entire buffer all at once
+        strategy = require('ts-rainbow').strategy.global,
+      }
+    end,
   },
 
   -- history
