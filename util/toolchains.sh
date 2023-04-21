fcea7d5add9a2f3b72b4f60d065227511eae1017
pwjworks <13610513466@163.com>
2023-04-22 01:36:42 +0800

    add python & autojump

--- util/toolchains.sh
+++ util/toolchains.sh
@@ -1,4 +1,11 @@
 cd ~
+
+# python
+sudo apt update && sudo apt upgrade
+sudo apt install build-essential libncursesw5-dev libssl-dev \
+libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+suto apt install python3
+
 # cmake & xmake
 wget https://github.com/Kitware/CMake/releases/download/v3.26.3/cmake-3.26.3-linux-x86_64.sh
 sudo zsh ./cmake-3.26.3-linux-x86_64.sh
@@ -14,3 +21,7 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
+
+# autojump
+git clone git://github.com/wting/autojump.git
+cd autojump
