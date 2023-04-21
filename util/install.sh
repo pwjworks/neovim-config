c431aba327c95b6089d832247bf1642b32552cb6
pwjworks <13610513466@163.com>
2023-04-22 01:24:32 +0800

    fix install script

--- util/install.sh
+++ util/install.sh
@@ -46,18 +46,4 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-
-# cmake & xmake
-wget https://github.com/Kitware/CMake/releases/download/v3.26.3/cmake-3.26.3-linux-x86_64.sh
-sudo cd ~ | zsh ./cmake-3.26.3-linux-x86_64.sh
-cd ~ | wget https://xmake.io/shget.text -O - | bash
-
-# nodejs
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-source ~/.zshrc
-
-# lazygit
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
+zsh ./toolchains.sh
