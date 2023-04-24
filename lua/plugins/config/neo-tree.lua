4f30841f1d7aee0242c4b26dc76c13804c99d1ad
pwjworks <13610513466@163.com>
2023-04-25 01:41:08 +0800

    add colorizer & change neotree settings

--- lua/plugins/config/neo-tree.lua
+++ lua/plugins/config/neo-tree.lua
@@ -99,4 +99,5 @@ local options = {
     },
   },
 }
+
 return options
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -7,7 +7,8 @@ return {
       "MunifTanjim/nui.nvim",
     },
     opts = function()
-      return require "plugins.config.neo-tree"
+      vim.api.nvim_set_hl(0, "NeoTreeCursorLine", {fg = "#D8EEEB", bg = "#58B5A8" })
+      return require("plugins.config.neo-tree")
     end
   },
 
@@ -314,6 +315,43 @@ return {
     'numToStr/Comment.nvim',
     config = function()
       require('Comment').setup()
+    end
+  },
+  {
+    'folke/todo-comments.nvim',
+    opts = {
+      highlight = {
+        multiline = true,                 -- enable multine todo comments
+        multiline_pattern = "^.",         -- lua pattern to match the next multiline from the start of the matched keyword
+        multiline_context = 10,           -- extra lines that will be re-evaluated when changing a line
+        before = "",                      -- "fg" or "bg" or empty
+        keyword = "wide",                 -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+        after = "fg",                     -- "fg" or "bg" or empty
+        pattern = [[.*<(KEYWORDS)\s*:?]], -- pattern or table of patterns, used for highlighting (vim regex)
+        comments_only = true,             -- uses treesitter to match keywords in comments only
+        max_line_len = 400,               -- ignore lines longer than this
+        exclude = {},                     -- list of file types to exclude highlighting
+      },
+      search = {
+        command = "rg",
+        args = {
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+        },
+        -- regex that will be used to match keywords.
+        -- don't replace the (KEYWORDS) placeholder
+        -- pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+        pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+      },
+    },
+  },
+  {
+    'norcalli/nvim-colorizer.lua',
+    config=function ()
+      require("colorizer").setup()
     end
   }
 }
